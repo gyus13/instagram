c4b9aff1a3cc8154e87a5c952bab890e41b95069
gyus13 <65529348+gyus13@users.noreply.github.com>
2022-02-16 11:05:05 +0900

    mongodb

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { LoggerMiddleware } from './logger.middleware';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot(),
+      MongooseModule.forRoot(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+        .apply(LoggerMiddleware)
+        .forRoutes('*');
+  }
+}
